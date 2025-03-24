a022c4cf0b96270ffba60889b461099343aa6904
Rubén <98332317+rubnMartnez@users.noreply.github.com>
2025-03-24 19:15:58 +0000

    add webEnum script

---
+++ webEnum.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Disclaimer: you have to have go installed and 
+# assetfinder, Amass cloned before running this script
+
+url=$1
+run_amass=false
+
+# Check for -a flag
+if [[ "$2" == "-a" ]]; then
+    run_amass=true
+fi
+
+# TODO add usage
+# TODO add depency checks and clone them if not there
+
+if [! -d "$url"];then
+    mkdir $url
+fi
+
+if [! -d "$url/recon"];then
+    mkdir $url/recon
+fi
+
+echo "[+] Harvesting subdomains with assetfinder..."
+
+assetfinder $url >> $url/recon/afAssets.txt
+cat $url/recon/assets.txt | grep $url >> $url/recon/assetsOnScope.txt
+
+# Run Amass only if -a flag is present
+if [ "$run_amass" == true ]; then
+    echo "[+] Harvesting subdomains with Amass..."
+    amass enum -d "$url" > "$url/recon/amassAssets.txt"
+    sort -u "$url/recon/amassAssets.txt" >> "$url/recon/assetsOnScope.txt"
+fi
+
+
+
+
+
+
+
+echo "[+] Recon finished!"
